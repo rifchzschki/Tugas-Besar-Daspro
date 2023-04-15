aae96f104439444d6a7c165f1fd51870ba1e3559
rifchzschki <114592995+rifchzschki@users.noreply.github.com>
2023-04-15 07:23:53 +0700

    Delete exit.py

--- exit.py
+++
@@ -1,9 +0,0 @@
-import os
-def exit():
-    yakin = input(f"Apakah Anda mau melakukan penyimpanan file yang sudah diubah (y/n)? ")
-    if (yakin == "Y") or (yakin == "y"):
-        pass#return ... (save)
-    else :
-        os._exit(1)
-
-exit()
