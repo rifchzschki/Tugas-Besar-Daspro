47baea52594363f722c0e0c5b79dd42b7e9e7f33
Wanzhr1 <shafhair16@gmail.com>
2023-04-16 19:41:06 +0700

    Create hancurkancandi.py
    Masih bingung gmn cara ngapus candi dr array nya

---
+++ modules/hancurkancandi.py
@@ -0,0 +1,17 @@
+
+hancurkan = int(input('Masukkan ID candi: '))
+YorN = False
+
+while YorN == False:
+    yakin = input(f'Apakah Anda yakin ingin menghancurkan candi ID: {hancurkan} (Y/N)? ')
+    if yakin == 'Y' or yakin=='y':
+        YorN = True
+        if  :
+        else:
+            print(f'Candi ID: {hancurkan} tidak ditemukan')
+        print(f'Candi ID: {hancurkan} berhasil dihancurkan')
+    elif yakin=='N' or yakin=='n':
+        print(f'Candi ID: {hancurkan} tidak jadi dihancurkan')
+        YorN = True
+    else:
+        continue
