053fa23aefab061f94454694a2ae1461ba03c097
Wanzhr1 <shafhair16@gmail.com>
2023-04-29 12:50:32 +0700

    Update hancurkancandi.py

--- modules/hancurkancandi.py
+++ modules/hancurkancandi.py
@@ -15,15 +15,15 @@ def hancurkancandi(candi):
         YorN = False
         if cek_candi(candi, hancurkan):
             while not YorN:
-            yakin = input(f'Apakah Anda yakin ingin menghancurkan candi ID: {hancurkan} (Y/N)? ')
-            if yakin == 'Y' or yakin=='y':
-                YorN = True
-                print(f'Candi ID: {hancurkan} berhasil dihancurkan')
-                candi[hancurkan] = ["none", "none", "none", "none", "none"]
-            elif yakin=='N' or yakin=='n':
-                print(f'Candi ID: {hancurkan} tidak jadi dihancurkan')
-                YorN = True
-            else:
-                continue
+                yakin = input(f'Apakah Anda yakin ingin menghancurkan candi ID: {hancurkan} (Y/N)? ')
+                if yakin == 'Y' or yakin=='y':
+                    YorN = True
+                    print(f'Candi ID: {hancurkan} berhasil dihancurkan')
+                    candi[hancurkan] = ["none", "none", "none", "none", "none"]
+                elif yakin=='N' or yakin=='n':
+                    print(f'Candi ID: {hancurkan} tidak jadi dihancurkan')
+                    YorN = True
+                else:
+                    continue
         else:
             print(f'Tidak ada candi ID: {hancurkan}')
